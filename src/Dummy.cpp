f527bd444a021928b0c6fea7886c6b80d2911241
trocolit2 <trocolit2@gmail.com>
2017-02-14 10:07:26 -0300

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace rgbd_slam;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <rgbd_slam/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    rgbd_slam::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
